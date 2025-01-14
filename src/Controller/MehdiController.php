15d6dbddc8c183d8de831989c6f37a2f23578035
XENAITT <mehdi.benyettou@etu.univ-st-etienne.fr>
2025-01-14 17:07:03 +0100

    /C:/Users/MEHDI/projet2_symfony/vendor

---
+++ src/Controller/MehdiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MehdiController extends AbstractController
+{
+    /**
+     * @Route("/mehdi", name="app_mehdi")
+     */
+    public function index(): Response
+    {
+        return $this->render('mehdi/index.html.twig', [
+            'controller_name' => 'MehdiController',
+        ]);
+    }
+    /**
+     * @Route("/blog/home",name="app_home")
+     */
+    public function home() : response
+    {
+        return $this->render('mehdi/home.html.twig', [
+            'title' => "Bienvenue",
+            'age' => 21
+        ]);
+    }
+}
