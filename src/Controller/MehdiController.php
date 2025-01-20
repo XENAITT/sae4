d672ceb62c31de384abb895d5e1254de20f3814a
XENAITT <mehdi.benyettou@etu.univ-st-etienne.fr>
2025-01-20 20:24:28 +0100

    sae4

--- src/Controller/MehdiController.php
+++ src/Controller/MehdiController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MehdiController extends AbstractController
@@ -48,6 +50,7 @@ class MehdiController extends AbstractController
             'title' => "E-Portfolio",
         ]);
     }
+
     /**
      * @Route("/cv-detail", name="app_cv_detail")
      */
@@ -57,6 +60,7 @@ class MehdiController extends AbstractController
             'title' => "CV Détail",
         ]);
     }
+
     /**
      * @Route("/moi", name="app_moi")
      */
@@ -65,5 +69,22 @@ class MehdiController extends AbstractController
         return $this->render('mehdi/moi.html.twig', [
             'title' => "En savoir plus sur moi",
         ]);
+    }
+
+    /**
+     * @Route("/download-cv", name="app_download_cv")
+     */
+    public function downloadCv(): Response
+    {
+        $filePath = $this->getParameter('kernel.project_dir') . '/public/cv/cv_Mehdi.pdf'; // Chemin vers votre fichier CV
+
+        if (!file_exists($filePath)) {
+            throw $this->createNotFoundException('Le fichier n\'existe pas.');
+        }
+
+        $response = new BinaryFileResponse($filePath);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'cv_Mehdi.pdf');
+
+        return $response;
     }
 }
